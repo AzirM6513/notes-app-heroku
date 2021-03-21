71ed7b02487202892c30099567d692d51bd0ef93
Mena Azir <AzirM6513@users.noreply.github.com>
2021-03-21 08:05:20 -0400

    server post bug fix

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(express.json());
-app.use(cors());
 app.use(express.static("build"));
+app.use(cors());
+app.use(express.json());
 
 let notes = [
   {
     id: 1,
     content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
+    date: "2020-01-10T17:30:31.098Z",
     important: true,
   },
   {
     id: 2,
     content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
+    date: "2020-01-10T18:39:34.091Z",
     important: false,
   },
   {
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
+    date: "2020-01-10T19:20:14.298Z",
     important: true,
   },
 ];
@@ -35,24 +35,12 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    return res.json(note);
-  }
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-  res.status(404).end();
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-  const id = req.params.id;
-  notes = notes.filter((note) => note.id !== id);
-
-  res.status(204).end();
-});
-
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (request, response) => {
   const body = request.body;
 
   if (!body.content) {
@@ -70,15 +58,32 @@ app.post("/api/notes", (req, res) => {
 
   notes = notes.concat(note);
 
-  res.json(note);
+  response.json(note);
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
 
-  return maxId + 1;
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
 };
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
